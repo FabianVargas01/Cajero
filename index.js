5b28d771778b234d23779bfc04b211015b8bac15
FabianVargas01 <favargasg@gmail.com>
2023-07-05 23:14:03 -0500

    Se agrega el Proyecto

---
+++ index.js
@@ -0,0 +1,20 @@
+const accounts = [
+    {
+        numberCard: "1",
+        headline: "Mali Romero",
+        balance: 200,
+        password: "1"
+    },
+    {
+        numberCard: "2",
+        headline: "Maui Fernandez",
+        balance: 67,
+        password: "2"
+    },
+    {
+        numberCard: "3",
+        headline: "Gera Contreras",
+        balance: 290,
+        password: "3"
+    },
+];
